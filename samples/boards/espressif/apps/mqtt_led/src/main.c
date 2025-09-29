11d9c4384b75a1154e4b3dc4830968fcf29fc4e2
Hei Weilu <heiweilu@outlook.com>
2025-09-29 16:39:25 +0800

    samples: esp32s3_devkitc: mqtt_led
    add a new sample app mqtt_led for esp32s3_devkitc board,
    which connects to a Wi-Fi network and an MQTT broker,
    subscribes to a topic, and controls an LED based on received messages.
    
    Signed-off-by: Hei Weilu <heiweilu@outlook.com>

---
+++ samples/boards/espressif/apps/mqtt_led/src/main.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Hei Weilu
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* Simple MQTT LED sample placeholder: will be expanded to connect Wi-Fi and MQTT. */
+#include <zephyr/kernel.h>
+
+void main(void)
+{
+	printk("Hello World! %s\n", CONFIG_BOARD);
+}
